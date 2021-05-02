b76e6a026ab166aefc260684530662ca4e26b701
Arun Donti <dontirun@gmail.com>
2021-05-01 21:01:32 -0400

    chore: use projen token for automation and disable dependabot

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,7 @@
 const { AwsCdkConstructLibrary } = require('projen');
+const { Automation } = require('projen-automate-it');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
   author: 'Amazon Web Services',
@@ -8,9 +11,8 @@ const project = new AwsCdkConstructLibrary({
   name: 'cdk-serverless-clamscan',
   repositoryUrl: 'https://github.com/awslabs/cdk-serverless-clamscan',
 
-  /* AwsCdkConstructLibraryOptions */
-  // cdkAssert: true,                                                          /* Install the @aws-cdk/assert library? */
-  cdkDependencies: [ /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
+
+  cdkDependencies: [
     '@aws-cdk/aws-cloudtrail',
     '@aws-cdk/aws-ec2',
     '@aws-cdk/aws-efs',
@@ -24,103 +26,31 @@ const project = new AwsCdkConstructLibrary({
     '@aws-cdk/aws-sqs',
     '@aws-cdk/core',
   ],
-  cdkTestDependencies: ['@aws-cdk/assert'], /* AWS CDK modules required for testing. */
-  // cdkVersionPinning: false,                                                 /* Use pinned version instead of caret version for CDK. */
-
-  /* ConstructLibraryOptions */
-  // catalog: undefined,                                                       /* Libraries will be picked up by the construct catalog when they are published to npm as jsii modules and will be published under:. */
-
-  /* JsiiProjectOptions */
-  // compat: false,                                                            /* Automatically run API compatibility test against the latest version published to npm after compilation. */
-  // compatIgnore: '.compatignore',                                            /* Name of the ignore file for API compatibility tests. */
-  docgen: true, /* Automatically generate API.md from jsii. */
-  eslint: true, /* Install eslint. */
-  // eslintOptions: undefined,                                                 /* Eslint options. */
-  // publishToGo: undefined,                                                   /* Publish Go bindings to a git repository. */
-  // publishToMaven: undefined                                                 /* Publish to maven. */
-  // publishToNuget: undefined                                                 /* Publish to NuGet. */
+  cdkTestDependencies: ['@aws-cdk/assert'],
+  docgen: true,
+  eslint: true,
   publishToPypi: {
     distName: 'cdk-serverless-clamscan',
     module: 'cdk_serverless_clamscan',
-  }, /* Publish to pypi. */
-  // rootdir: '.',                                                             /* undefined */
+  },
 
-  /* NodePackageOptions */
-  // allowLibraryDependencies: true,                                           /* Allow the project to include `peerDependencies` and `bundledDependencies`. */
-  // authorOrganization: undefined,                                            /* Author's Organization. */
-  // authorUrl: undefined,                                                     /* Author's URL / Website. */
-  // autoDetectBin: true,                                                      /* Automatically add all executables under the `bin` directory to your `package.json` file under the `bin` section. */
-  bin: ['./assets'], /* Binary programs vended with your module. */
-  // bundledDeps: undefined,                                                   /* List of dependencies to bundle into this module. */
-  // deps: [],                                                                 /* Runtime dependencies of this module. */
-  description: 'Serverless architecture to virus scan objects in Amazon S3.', /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                                                              /* Build dependencies for this module. */
-  // entrypoint: 'lib/index.js',                                               /* Module entrypoint (`main` in `package.json`). */
-  // homepage: undefined,                                                      /* Package's Homepage / Website. */
-  keywords: ['clamav', 'virus scan', 'aws', 'docker', 'serverless', 'lambda', 's3', 'efs', 'eventbridge', 'sqs'], /* Keywords to include in `package.json`. */
-  license: 'Apache-2.0', /* License's SPDX identifier. */
-  // licensed: true,                                                           /* Indicates if a license should be added. */
-  // maxNodeVersion: undefined,                                                /* Minimum node.js version to require via `engines` (inclusive). */
-  // minNodeVersion: undefined,                                                /* Minimum Node.js version to require via package.json `engines` (inclusive). */
-  // npmAccess: undefined,                                                     /* Access level of the npm package. */
-  // npmDistTag: 'latest',                                                     /* Tags can be used to provide an alias instead of version numbers. */
-  // npmRegistryUrl: 'https://registry.npmjs.org',                             /* The base URL of the npm package registry. */
-  // npmTaskExecution: NpmTaskExecution.PROJEN,                                /* Determines how tasks are executed when invoked as npm scripts (yarn/npm run xyz). */
-  // packageManager: NodePackageManager.YARN,                                  /* The Node Package Manager used to execute scripts. */
-  // packageName: undefined,                                                   /* The "name" in package.json. */
-  // peerDependencyOptions: undefined,                                         /* Options for `peerDeps`. */
-  // peerDeps: [],                                                             /* Peer dependencies for this module. */
-  // projenCommand: 'npx projen',                                              /* The shell command to use in order to run the projen CLI. */
-  // repository: undefined,                                                    /* The repository is the location where the actual code for your package lives. */
-  // repositoryDirectory: undefined,                                           /* If the package.json for your package is not in the root directory (for example if it is part of a monorepo), you can specify the directory in which it lives. */
-  // scripts: {},                                                              /* npm scripts to include. */
-  // stability: undefined,                                                     /* Package's Stability. */
-
-  /* NodeProjectOptions */
-  // antitamper: true,                                                         /* Checks that after build there are no modified files on git. */
-  // buildWorkflow: undefined,                                                 /* Define a GitHub workflow for building PRs. */
-  // codeCov: false,                                                           /* Define a GitHub workflow step for sending code coverage metrics to https://codecov.io/ Uses codecov/codecov-action@v1 A secret is required for private repos. Configured with @codeCovTokenSecret. */
-  // codeCovTokenSecret: undefined,                                            /* Define the secret name for a specified https://codecov.io/ token A secret is required to send coverage for private repositories. */
-  // copyrightOwner: undefined,                                                /* License copyright owner. */
-  // copyrightPeriod: undefined,                                               /* The copyright years to put in the LICENSE file. */
-  defaultReleaseBranch: 'main', /* The name of the main release branch. */
-  // dependabot: true,                                                         /* Include dependabot configuration. */
-  // dependabotOptions: undefined,                                             /* Options for dependabot. */
-  gitignore: ['.vscode/', '.venv/'], /* Additional entries to .gitignore. */
-  // jest: true,                                                               /* Setup jest unit tests. */
-  // jestOptions: undefined,                                                   /* Jest options. */
-  // mergify: true,                                                            /* Adds mergify configuration. */
-  // mergifyAutoMergeLabel: 'auto-merge',                                      /* Automatically merge PRs that build successfully and have this label. */
-  // mergifyOptions: undefined,                                                /* Options for mergify. */
-  // npmignore: undefined,                                                     /* Additional entries to .npmignore. */
-  // npmignoreEnabled: true,                                                   /* Defines an .npmignore file. Normally this is only needed for libraries that are packaged as tarballs. */
-  // projenDevDependency: true,                                                /* Indicates of "projen" should be installed as a devDependency. */
-  // projenUpgradeAutoMerge: undefined,                                        /* Automatically merge projen upgrade PRs when build passes. */
-  // projenUpgradeSchedule: [ '0 6 * * *' ],                                   /* Customize the projenUpgrade schedule in cron expression. */
-  // projenUpgradeSecret: undefined,                                           /* Periodically submits a pull request for projen upgrades (executes `yarn projen:upgrade`). */
-  // projenVersion: Semver.latest(),                                           /* Version of projen to install. */
-  // pullRequestTemplate: true,                                                /* Include a GitHub pull request template. */
-  // pullRequestTemplateContents: undefined,                                   /* The contents of the pull request template. */
-  // rebuildBot: undefined,                                                    /* Installs a GitHub workflow which is triggered when the comment "@projen rebuild" is added to a pull request. */
-  // rebuildBotCommand: 'rebuild',                                             /* The pull request bot command to use in order to trigger a rebuild and commit of the contents of the branch. */
-  // releaseBranches: [ 'master' ],                                            /* Branches which trigger a release. */
-  // releaseEveryCommit: true,                                                 /* Automatically release new versions every commit to one of branches in `releaseBranches`. */
-  // releaseSchedule: undefined,                                               /* CRON schedule to trigger new releases. */
-  // releaseToNpm: false,                                                      /* Automatically release to npm when new versions are introduced. */
-  // releaseWorkflow: undefined,                                               /* Define a GitHub workflow for releasing from "master" when new versions are bumped. */
-  // workflowBootstrapSteps: 'yarn install --frozen-lockfile && yarn projen',  /* Workflow steps to use in order to bootstrap this repo. */
-  // workflowContainerImage: undefined,                                        /* Container image to use for GitHub workflows. */
-  // workflowNodeVersion: undefined,                                           /* The node version to use in GitHub workflows. */
-
-  /* ProjectOptions */
-  // clobber: true,                                                            /* Add a `clobber` task which resets the repo to origin. */
-  // devContainer: false,                                                      /* Add a VSCode development environment (used for GitHub Codespaces). */
-  // gitpod: false,                                                            /* Add a Gitpod development environment. */
-  // logging: {},                                                              /* Configure logging options such as verbosity. */
-  // outdir: '.',                                                              /* The root directory of the project. */
-  // parent: undefined,                                                        /* The parent project, if this project is part of a bigger project. */
-  // projectType: ProjectType.UNKNOWN,                                         /* Which type of project this is (library/app). */
-  // readme: undefined,                                                        /* The README setup. */
+  bin: ['./assets'],
+  devDeps: ['projen-automate-it'],
+  description: 'Serverless architecture to virus scan objects in Amazon S3.',
+  keywords: ['clamav', 'virus scan', 'aws', 'docker', 'serverless', 'lambda', 's3', 'efs', 'eventbridge', 'sqs'],
+  license: 'Apache-2.0',
+  defaultReleaseBranch: 'main',
+  dependabot: false,
+  gitignore: ['.vscode/', '.venv/', 'cdk.out', 'cdk.context.json', 'dockerAssets.d', 'yarn-error.log'],
 });
+
+const automation = new Automation(project, {
+  automationToken: AUTOMATION_TOKEN,
+});
+
+automation.autoApprove();
+automation.autoMerge();
+automation.projenYarnUpgrade();
+automation.projenYarnUpgrade('projenYarnUpgradeWithTest', { yarnTest: true });
 
 project.synth();
